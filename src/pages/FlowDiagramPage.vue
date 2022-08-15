4e1def6061f3655f39c5ef8f5744659e410326b5
terry54147 <0930164@gmail.com>
2022-08-15 23:07:46 +0800

    add node elmemnt in random position

--- src/pages/FlowDiagramPage.vue
+++ src/pages/FlowDiagramPage.vue
@@ -9,26 +9,40 @@
 
 <script setup lang="ts">
 import { reactive } from 'vue';
+import { uid } from 'quasar';
+import _ from 'lodash';
+import { Range } from 'src/models/models';
 import type { Elements } from '@braks/vue-flow';
 import FlowDiagram from 'components/FlowDiagram.vue';
+import  store, {useStore}  from '../store/index';
 
-const elements = reactive<Elements>([
-  {
-    id: '1',
-    type: 'input',
-    label: 'Node 1',
-    position: { x: 250, y: 5 },
-    class: 'light',
-  },
-  { id: '2', label: 'Node 2', position: { x: 100, y: 100 }, class: 'light' },
-  { id: '3', label: 'Node 3', position: { x: 400, y: 100 }, class: 'light' },
-  { id: '4', label: 'Node 4', position: { x: 400, y: 200 }, class: 'light' },
-  { id: 'e1-2', source: '1', target: '2', animated: true },
-  { id: 'e1-3', source: '1', target: '3' },
-]);
+const elements = reactive<Elements>([]);
 
 const addItem = () => {
   console.log('addItem');
+  const id = uid();
+  console.log('uid: ' + uid);
+  const label = getLabel();
+  const position = getPosition({ min: 5, max: 300 }, { min: 10, max: 200 });
+  const node = {
+    id,
+    label,
+    position,
+    type: _.isEmpty(elements) ? 'input' : '',
+  };
+  elements.push(node);
+  // test
+  useStore();
+  console.log(store);
+
+};
+
+const getLabel = () => `Layer ${elements.length + 1}`;
+const getPosition = (xRange: Range, yRange: Range) => {
+  return {
+    x: _.random(xRange.min, xRange.max),
+    y: _.random(yRange.min, yRange.max),
+  };
 };
 </script>
 
